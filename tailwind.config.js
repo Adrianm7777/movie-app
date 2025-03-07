74d1f8c10da5e908751082124c282629aadb3e11
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-03-07 18:44:34 +0100

    Feature: Add Tailwind CSS configuration and update project dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {},
+  fontFamily: {
+    sans: ["Arial", "Helvetica", "sans-serif"],
+    mono: ["monospace"],
+  },
+
+  plugins: [],
+};
